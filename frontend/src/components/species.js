a3ad63533664e9064b7a37ada4a2ad59e92b195f
Sam Markham <sam.e.markham@gmail.com>
2020-02-10 15:56:18 -0800

    Adds fetchAndLoadSpecies method to species class

--- frontend/src/components/species.js
+++ frontend/src/components/species.js
@@ -3,5 +3,12 @@ class Species {
     this.species = []
     this.adapter = new SpeciesAdapter()
     // this.bindEventListeners()
+    this.fetchAndLoadSpecies()
+  }
+
+  fetchAndLoadSpecies() {
+    this.adapter.getSpecies().then(species => {
+      console.log(species)
+    })
   }
 }
