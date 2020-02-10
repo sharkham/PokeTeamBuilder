dc3e5423f23fbb3e170415e69fbcaf5baf07546f
Sam Markham <sam.e.markham@gmail.com>
2020-02-10 15:32:58 -0800

    Adds sprite images to public directory in api

---
+++ poketeam-builder-api/app/controllers/api/v1/species_controller.rb
@@ -0,0 +1,22 @@
+class Api::V1::SpeciesController < ApplicationController
+  def index
+    @species = Species.all
+    render json: @species, status: 200
+  end
+
+  def show
+    @species = Species.find(params[:id])
+    render json: @species, status: 200
+  end
+
+  def create
+    @species = Species.create(species_params)
+    render json: @species, status: 200
+  end
+
+  private
+
+  def species_params
+    params.require(:species).permit
+  end
+end
