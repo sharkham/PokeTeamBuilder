4b8c8852d5c20494a2e8ec54c9307821cbf1773e
Sam Markham <sam.e.markham@gmail.com>
2020-02-11 14:56:45 -0800

    Adds fetch and load trainer method to trainers class

--- frontend/src/components/trainers.js
+++ frontend/src/components/trainers.js
@@ -1,11 +1,21 @@
 class Trainers {
   constructor(){
-    this.trainers = []
+    this.trainer = []
     this.adapter = new TrainersAdapter()
     this.initBindingsAndEventListeners()
     this.fetchAndLoadTrainer()
+    //this just fires when the thing starts. Going to have to find a way to login/signup first to fire it.
   }
 
-
+  fetchAndLoadTrainer() {
+    this.adapter.getTrainer(1)
+    .then(trainer => {
+      this.trainer = new Trainer(trainer)
+      // trainers.forEach(entry => this.trainers.push(new Entry(entry)))
+    })
+    .then(() => {
+      this.render()
+    })
+  }
 
 }
