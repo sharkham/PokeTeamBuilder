8f295b73017b85346251f5cb2da8b526385e2065
Sam Markham <sam.e.markham@gmail.com>
2020-02-11 14:30:44 -0800

    Adds trainers controller

---
+++ poketeam-builder-api/app/controllers/api/v1/trainers_controller.rb
@@ -0,0 +1,24 @@
+class Api::V1::PokemonsController < ApplicationController
+
+  def index
+    trainers = Trainer.all
+    render json: TrainerSerializer.new(trainers).to_serialized_json, status: 200
+  end
+
+  def show
+    trainer = Trainer.find_by(params[:id])
+    render json: TrainerSerializer.new(trainer).to_serialized_json, status: 200
+  end
+
+  def create
+    trainer = Trainer.create(trainer_params)
+    render json: TrainerSerializer.new(trainer).to_serialized_json, status: 200
+  end
+
+  private
+
+  def trainer_params
+    params.require(:trainer).permit(:id, :name, :image, :height, :xaxis, :yaxis, :zindex)
+  end
+
+end
