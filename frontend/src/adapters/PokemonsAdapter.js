1b90efd816abfd0efa81a538086026df61abf267
Sam Markham <sam.e.markham@gmail.com>
2020-02-11 13:31:46 -0800

    Adds create Pokemon method to Pokemons Adapter

--- frontend/src/adapters/PokemonsAdapter.js
+++ frontend/src/adapters/PokemonsAdapter.js
@@ -6,4 +6,15 @@ class PokemonsAdapter {
   // getEntries() {
   //   return fetch(this.baseUrl).then(res => res.json())
   // }
+
+  // createPokemon(value) {
+  //   const pokemon = {
+  //     //find by id? Or find by name if I change the value
+  //     //then set all properties here
+  //   }
+  //   return fetch(this.baseURL, {
+  //     method: "POST",
+  //     body: JSON.stringify({ pokemon: pokemon })
+  //   })
+  // }
 }
