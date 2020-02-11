b0b435361dd70689786c3da4ba0998adaff079c6
Sam Markham <sam.e.markham@gmail.com>
2020-02-11 14:20:45 -0800

    Adds pokemon serializer

---
+++ poketeam-builder-api/app/serializers/pokemon_serializer.rb
@@ -0,0 +1,17 @@
+class PokemonSerializer
+  # include FastJsonapi::ObjectSerializer
+  # attributes :nickname, :species, :trainer_id
+  # # belongs_to :trainer
+
+  def initialize(pokemon_object)
+    @pokemon = pokemon_object
+  end
+
+  def to_serialized_json
+    options = {
+      only: [:id, :name, :image, :height, :xaxis, :yaxis, :zindex, :trainer_id]
+      # include: [:id, :name]
+    }
+    @pokemon.to_json(options)
+  end
+end
