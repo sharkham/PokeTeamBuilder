c4e8df267499fd96d521d0ee5fc0713edd5602bd
Sam Markham <sam.e.markham@gmail.com>
2020-02-11 15:17:09 -0800

    Adds pokemons to Trainer constructor

--- frontend/src/components/trainer.js
+++ frontend/src/components/trainer.js
@@ -4,6 +4,7 @@ class Trainer {
     this.name = entryJSON.name
     this.image = entryJSON.image
     this.height = entryJSON.height
+    this.pokemons = entryJSON.pokemons
 
     //set these to defaults on render, somehow depending on which select they come from
     // this.xaxis =
