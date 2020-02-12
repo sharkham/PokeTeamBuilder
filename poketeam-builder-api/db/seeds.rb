bd6b6a12d3a90f907899dc777a08c111fa0ef73a
Sam Markham <sam.e.markham@gmail.com>
2020-02-12 10:12:27 -0800

    Adds all Pokémon from Pokémon API to seed file to make Entries

--- poketeam-builder-api/db/seeds.rb
+++ poketeam-builder-api/db/seeds.rb
@@ -22,22 +22,54 @@
 #   species.save
 # end
 
-Entry.create(name: "Bulbasaur", height: 7, image: "image/1.png")
-Entry.create(name: "Ivysaur", height: 10, image: "image/2.png")
-Entry.create(name: "Venusaur", height: 20, image: "image/3.png")
-Entry.create(name: "Charmander", height: 6, image: "image/4.png")
-Entry.create(name: "Charmeleon", height: 11, image: "image/5.png")
-Entry.create(name: "Charizard", height: 17, image: "image/6.png")
-Entry.create(name: "Squirtle", height: 5, image: "image/7.png")
-Entry.create(name: "Wartortle", height: 10, image: "image/8.png")
-Entry.create(name: "Blastoise", height: 16, image: "image/9.png")
+require 'rest-client'
+require 'pry'
+
+pokedex_url = 'https://pokeapi.co/api/v2/pokemon/'
+
+pokedex = RestClient.get 'https://pokeapi.co/api/v2/pokemon/'
+
+386.times do |i|
+  num = i+1
+  pokedex_entry = RestClient.get "https://pokeapi.co/api/v2/pokemon/#{num}"
+  pokedex_entry_json = JSON.parse(pokedex_entry)
+  Entry.create(
+    name: pokedex_entry_json["name"].capitalize, #capitalize this somewhere
+    height: pokedex_entry_json["height"],
+    image: "image/#{num}.png"
+  )
+  # binding.pry
+end
+
+#loop from 1-386, for each number, grab the URL from that
+
+# rm_array = JSON.parse(rm)[“results”]
+# rm_array.each do |character|
+#   Character.create(
+#   name: character[“name”],
+#   status: character[“status”],
+#   species: character[“species”],
+#   gender: character[“gender”],
+#   image: character[“image”]
+#   )
+#  end
+
+# Entry.create(name: "Bulbasaur", height: 7, image: "image/1.png")
+# Entry.create(name: "Ivysaur", height: 10, image: "image/2.png")
+# Entry.create(name: "Venusaur", height: 20, image: "image/3.png")
+# Entry.create(name: "Charmander", height: 6, image: "image/4.png")
+# Entry.create(name: "Charmeleon", height: 11, image: "image/5.png")
+# Entry.create(name: "Charizard", height: 17, image: "image/6.png")
+# Entry.create(name: "Squirtle", height: 5, image: "image/7.png")
+# Entry.create(name: "Wartortle", height: 10, image: "image/8.png")
+# Entry.create(name: "Blastoise", height: 16, image: "image/9.png")
 
 red = Trainer.create(name: "Red", height: 15)
 blue = Trainer.create(name: "Blue", height: 16)
 
-red.pokemons.build(name: "Bulbasaur", number: 1, height: 7)
-red.pokemons.build(name: "Ivysaur", number: 2, height: 10)
-red.save
+# red.pokemons.build(name: "Bulbasaur", number: 1, height: 7)
+# red.pokemons.build(name: "Ivysaur", number: 2, height: 10)
+# red.save
 
 blue.pokemons.build(name: "Blastoise", number: 1, height: 16)
 blue.pokemons.build(name: "Wartortle", number: 2, height: 10)
@@ -45,5 +77,3 @@ blue.pokemons.build(name: "Charizard", number: 3, height: 17)
 blue.save
 
 
-
-Pokemon.create(name: "Bulbasaur", height: 7)
