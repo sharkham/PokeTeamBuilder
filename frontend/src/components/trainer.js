b7e39b4f54a4c9a38cdbd3b367a4cbcbfea54b8e
Sam Markham <sam.e.markham@gmail.com>
2020-02-14 12:16:03 -0800

    Adds method to render trainer in control box when trainer is loaded

--- frontend/src/components/trainer.js
+++ frontend/src/components/trainer.js
@@ -15,6 +15,7 @@ class Trainer {
   initBindingsAndEventListeners() {
     this.h2 = document.getElementById("trainername")
     this.view = document.getElementById("view-box")
+    this.trainerControlBox = document.getElementById("control-trainer")
   }
 
   renderTrainer() {
@@ -22,11 +23,17 @@ class Trainer {
     this.h2.setAttribute("id", "trainername")
     this.h2.setAttribute("trainerid", `${this.id}`)
     this.renderTrainerInViewBox()
+    this.renderTrainerInControlBox()
     // this.pokemons = new Pokemons(this.entries, this.trainer)
   }
 
   renderTrainerInViewBox() {
     this.view.innerHTML += `<img src="${this.image}">`
+  }
+
+  renderTrainerInControlBox() {
+    this.trainerControlBox.innerHTML = `<img src="${this.image}">`
+    //change later to be smaller trainer icon
   }
 
   renderTrainersPokemons(entries) {
