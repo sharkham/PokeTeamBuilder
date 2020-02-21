131f8a54a3be58bee5b33bc7e4b326e5ea115565
Sam Markham <sam.e.markham@gmail.com>
2020-02-20 17:23:32 -0800

    Sets select form to display trainer image id for current trainer sprite on load

--- frontend/src/components/trainer.js
+++ frontend/src/components/trainer.js
@@ -16,6 +16,7 @@ class Trainer {
     this.h2 = document.getElementById("trainername")
     this.view = document.getElementById("view-box")
     this.trainerControlBox = document.getElementById("control-trainer")
+    this.trainerSelect = document.getElementById("trainerspriteselect")
   }
 
   renderTrainer() {
@@ -41,6 +42,10 @@ class Trainer {
 
   renderTrainerInControlBox() {
     this.trainerControlBox.innerHTML = `<img src="${this.image}">`
+    const array = this.image.split(/[^a-zA-Z\d\s]/)
+    const imageId = array[2]
+    // const imageId2 = imageId[2]
+    this.trainerSelect.value = imageId
     //change later to be smaller trainer icon
   }
 
