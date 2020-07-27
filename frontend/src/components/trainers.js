5e5149af77174daba632569ce60295e8eb3499f9
Sam Markham <sam.e.markham@gmail.com>
2020-07-27 16:48:38 -0700

    Fixes closeDropDown method so it closes both Create and Load menus when user clicks away from them

--- frontend/src/components/trainers.js
+++ frontend/src/components/trainers.js
@@ -201,32 +201,45 @@ class Trainers {
     // if event target is NOT dropdownLoginButton or dropdownLoginDiv, if dropdownLoginDiv is showing, hide it
     //if event target is NOT dropdownSignupButton or dropdownSignupDiv, if dropdownSignupDiv is showing, hide it
 
+    if (!e.target.matches('.dropdownLogin')) {
+      let dropdownLoginDiv = document.getElementById("dropdownLoginDiv");
+      if (dropdownLoginDiv.classList.contains('show')) {
+        dropdownLoginDiv.classList.remove('show');
+      }
+    }
+    if (!e.target.matches('.dropdownSignup')) {
+      let dropdownSignupDiv = document.getElementById("dropdownSignupDiv");
+      if (dropdownSignupDiv.classList.contains('show')) {
+        dropdownSignupDiv.classList.remove('show');
+      }
+    }
+
     // the code below works for doing stuff for the Login only--it doesn't work for the Signup.
     // if (!e.target.matches('#dropdownLoginButton') && !e.target.matches('#dropdownLoginDiv form') && !e.target.matches('#dropdownLoginDiv form input') && !e.target.matches('#dropdownLoginDiv form button')) {
 
     // this code works for everything but closing the one dropdown if you've clicked on the other one.
-    if (!e.target.matches('.dropdown')) {
-      let dropdownLoginDiv = document.getElementById("dropdownLoginDiv");
-      let dropdownSignupDiv = document.getElementById("dropdownSignupDiv");
-      // let aboutDiv = etc
-      console.log(e.target)
-      if (dropdownSignupDiv.classList.contains('show') && dropdownLoginDiv.classList.contains('show')) {
-        dropdownLoginDiv.classList.remove('show');
-        dropdownSignupDiv.classList.remove('show');
-        // console.log(e.target)
-      }  else if (dropdownSignupDiv.classList.contains('show')) {
-        dropdownSignupDiv.classList.remove('show');
-        // console.log(e.target)
-      } else if (dropdownLoginDiv.classList.contains('show')) {
-        dropdownLoginDiv.classList.remove('show');
-
-      }
-    // } else if (!e.target.matches('#dropdownSignupButton')) {
+    // if (!e.target.matches('.dropdown')) {
+    //   let dropdownLoginDiv = document.getElementById("dropdownLoginDiv");
     //   let dropdownSignupDiv = document.getElementById("dropdownSignupDiv");
-    //   if (dropdownSignupDiv.classList.contains('show')) {
+    //   // let aboutDiv = etc
+    //   console.log(e.target)
+    //   if (dropdownSignupDiv.classList.contains('show') && dropdownLoginDiv.classList.contains('show')) {
+    //     dropdownLoginDiv.classList.remove('show');
     //     dropdownSignupDiv.classList.remove('show');
+    //     // console.log(e.target)
+    //   }  else if (dropdownSignupDiv.classList.contains('show')) {
+    //     dropdownSignupDiv.classList.remove('show');
+    //     // console.log(e.target)
+    //   } else if (dropdownLoginDiv.classList.contains('show')) {
+    //     dropdownLoginDiv.classList.remove('show');
+
     //   }
-    }
+    // // } else if (!e.target.matches('#dropdownSignupButton')) {
+    // //   let dropdownSignupDiv = document.getElementById("dropdownSignupDiv");
+    // //   if (dropdownSignupDiv.classList.contains('show')) {
+    // //     dropdownSignupDiv.classList.remove('show');
+    // //   }
+    // }
   }
   // Close the dropdown if the user clicks outside of it
   // window.onclick = function(e) {
